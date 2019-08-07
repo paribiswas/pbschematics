5cde4e98c76ee321c02d67f3cadb46e0955f7c0b
paribiswas <50815597+paribiswas@users.noreply.github.com>
2019-08-06 22:02:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 variable "softlayer_username" {}
 variable "softlayer_api_key" {}
+variable "resource_group" {}
 
 provider "ibm" {
   region = "${var_region_name}"
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key  = "${var.softlayer_api_key}"
+}
+
+resource "ibm_resource_group" "resourceGroup"{
+  name     = "${var.resource_group}"
 }
