d0762d93958393842ad3e33d8f57cc0e3d4b8c7c
paribiswas <50815597+paribiswas@users.noreply.github.com>
2019-08-06 22:42:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,21 @@ provider "ibm" {
 resource "ibm_resource_group" "resourceGroup"{
   name     = "${var.resource_group}"
 }
+
+data "ibm_space" "spacedata" {
+  space = "${var.space}"
+  org   = "${var.org}"
+}
+
+resource "ibm_service_instance" "service-instance" {
+  name       = "${var.service_instance_name}"
+  space_guid = "${data.ibm_space.spacedata.id}"
+  service    = "${var.service}"
+  plan       = "${var.plan}"
+  tags       = ["cluster-service", "cluster-bind"]
+}
+
+resource "ibm_service_key" "serviceKey" {
+  name                  = "${var.service_key_name}"
+  service_instance_guid = "${ibm_service_instance.service-instance.id}"
+}
